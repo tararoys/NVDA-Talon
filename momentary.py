d2d63b3c857c6b02da946d65ac84b2a18a9faaf1
tararoys <tlroys@gmail.com>
2022-12-27 16:48:48 -0600

    Update momentary.py
    Copied the right file this time.  Momentary.py from aegis

--- momentary.py
+++ momentary.py
@@ -1,55 +1,28 @@
-import os
-import platform
-import pprint
-import re
-from itertools import islice
-from typing import Union
-
-from talon import Module, actions, app, clip, registry, scope, speech_system, ui
-from talon.grammar import Phrase
-
+from talon import Module, actions, speech_system, scope
+​
+flag = False
+def on_phrase_post(j):
+    global flag
+    if flag:
+        flag = False
+        checked_modes = list(scope.get('mode').intersection({'sleep', 'dictation'}))
+        # make sure we're in sleep or dictation mode:
+        if len(checked_modes) == 1:
+            mode = checked_modes[0]
+            actions.mode.enable('command')
+            actions.mode.disable(mode)
+            try:
+                # NOTE: the following API is completely private and subject to change with no notice
+                speech_system._on_audio_frame(j['samples'])
+            finally:
+                actions.mode.disable('command')
+                actions.mode.enable(mode)
+speech_system.register('post:phrase', on_phrase_post)
+​
 mod = Module()
-
-import csv
-from pathlib import Path
-
-from talon import resource
-
-# NOTE: This method requires this module to be one folder below the top-level
-#   knausj folder.
-SETTINGS_DIR = Path(__file__).parents[1] / "settings"
-
-if not SETTINGS_DIR.is_dir():
-    os.mkdir(SETTINGS_DIR)
-
 @mod.action_class
 class Actions:
-    def talon_locate_rule(phrase: Union[str, Phrase]):
-        """locates the file, rule, and line number where a particular voice command is defined, and copies it to the clipboard so NVDA can speak it"""
-        print("**** Simulated Phrse **** ")
-        location = speech_system._sim(str(phrase))
-        print(location)
-        filepath=location.partition("path: ")[2].split('\n')[0]
-        print(filepath)
-
-        rule_to_locate = location.partition("rule: \"")[2].split("\"")[0]
-        number = ""
-        with open(filepath) as myFile:
-            for num, line in enumerate(myFile, 1):
-                if rule_to_locate in line: 
-                    number = num
-                    print('found at line', num)
-        complete_location = location + "\n\t line number " + str(number)
-        print(complete_location)
-        print("*************************")
-        clip.set_text(complete_location) # sets the result to the clipboard
-        actions.sleep("10ms")
-        actions.key("insert:down c insert:up")   #makes nvda read the clipboard so that you know the file name, the written rule, and the line number the rule is on so that you can find it easily.       
-
-#actions.path.talon_user()
-
-def start_talon_in_sleep_mode():
-    """when talon starts up, automatically puts it into sleep mode, so that NVDA screen reader will not inadvertantly give it orders as NVDA reads things out while Talon is listening"""
-    actions.speech.disable()
-
-app.register("ready", start_talon_in_sleep_mode)
+    def momentary():
+        """Wake up Talon and re-run the entire current audio"""
+        global flag
+        flag = True
